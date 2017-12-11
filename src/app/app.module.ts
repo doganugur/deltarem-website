ad3c903f803084fab64a31236226e1d4e4c8a549
Dogan UGUR <doganu@gmail.com>
2017-12-11 20:41:47 +0000

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BlogComponent } from './blog/blog.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    TopMenuComponent,
+    MainComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    BlogComponent,
+    ServicesComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule

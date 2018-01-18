92f9e493d384b1b77e21008662076ca8e1b66a81
Dogan Ugur <dogan.ugur@markit.com>
2018-01-18 17:46:44 +0000

    update

--- src/app/shared/carousel/carousel.component.ts
+++ src/app/shared/carousel/carousel.component.ts
@@ -26,59 +26,6 @@ export class CarouselComponent implements OnInit, OnDestroy {
   }
 
 
-  public select(nextSlideIndex, direction: Direction = Direction.UNKNOWN) {
-      const nextIndex = nextSlideIndex;
-      if (direction === Direction.UNKNOWN) {
-          direction = nextIndex > this.getCurrentIndex() ? Direction.NEXT : Direction.PREV;
-      }
-
-      // Prevent this user-triggered transition from occurring if there is already one in progress
-      if (nextSlideIndex !== this.currentSlideIndex) {
-          this.goNext(nextSlideIndex, direction);
-      }
-  }
-
-  private goNext(slideIndex: number, direction: Direction) {
-      if (this.destroyed) {
-          return;
-      }
-
-      this.currentSlideIndex = slideIndex;
-  }
-
-  private getSlideByIndex(index: number) {
-      const len = this.slides.length;
-      for (let i = 0; i < len; ++i) {
-          if (this.slides[i].index === index) {
-              return this.slides[i];
-          }
-      }
-  }
-
-  private getCurrentIndex() {
-      return !this.currentSlideIndex ? 0 : this.currentSlideIndex;
-  }
-
-  private next() {
-      const newIndex = (this.getCurrentIndex() + 1) % this.slides.length;
-
-      if (newIndex === 0 && this.noWrap) {
-          return;
-      }
-
-      return this.select(this.getSlideByIndex(newIndex), Direction.NEXT);
-  }
-
-  private prev() {
-    const newIndex = this.getCurrentIndex() - 1 < 0 ? this.slides.length - 1 : this.getCurrentIndex() - 1;
-
-      if (this.noWrap && newIndex === this.slides.length - 1) {
-          return;
-      }
-
-      return this.select(this.getSlideByIndex(newIndex), Direction.PREV);
-  }
-
 
 }
 
